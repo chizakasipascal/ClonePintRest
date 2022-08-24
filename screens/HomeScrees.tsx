43a1f17c714fea990742d65aaca7a912c510e411
chizakasipascal <pascalkasichiza@gmail.com>
2022-08-24 10:30:44 +0200

    loop all in pins object disting to index use  modulo

--- screens/HomeScrees.tsx
+++ screens/HomeScrees.tsx
@@ -16,15 +16,27 @@ export default function HomeScreen({navigation}: RootTabScreenProps<'TabOne'>) {
       /> */}
         {/* Fisrt column */}
         <View style={{backgroundColor: "#94ffa955", flex: 1}}>
-          <Pin pin={pins[0]} />
+          {/* <Pin pin={pins[0]} />
           <Pin pin={pins[2]} />
-          <Pin pin={pins[4]} />
+          <Pin pin={pins[4]} /> */}
+
+          {/* Let loop this object */}
+
+          {pins.filter((item, index) => index % 2 == 0).map(
+            (pin) => (
+              <Pin pin={pin} />
+            ))}
         </View>
         {/* Second column */}
         <View style={{backgroundColor: "#cd94ff", flex: 1}}>
-          <Pin pin={pins[1]} />
+          {/* <Pin pin={pins[1]} />
           <Pin pin={pins[3]} />
-          <Pin pin={pins[5]} />
+          <Pin pin={pins[5]} /> */}
+
+          {pins.filter((item, index) => index % 2 == 1).map(
+            (pin) => (
+              <Pin pin={pin} />
+            ))}
         </View>
       </View>
 
